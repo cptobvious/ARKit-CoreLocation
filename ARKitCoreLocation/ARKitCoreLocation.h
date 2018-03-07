ae0e4d4168cd86f53d2e8f915deddfae6cf02cce
Chris Blättermann <cb@riskmethods.net>
2018-03-07 23:43:40 +0100

    build framework

---
+++ ARKitCoreLocation/ARKitCoreLocation.h
@@ -0,0 +1,19 @@
+//
+//  ARKitCoreLocation.h
+//  ARKitCoreLocation
+//
+//  Created by Chris Blättermann on 07.03.18.
+//  Copyright © 2018 Project Dent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ARKitCoreLocation.
+FOUNDATION_EXPORT double ARKitCoreLocationVersionNumber;
+
+//! Project version string for ARKitCoreLocation.
+FOUNDATION_EXPORT const unsigned char ARKitCoreLocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARKitCoreLocation/PublicHeader.h>
+
+
